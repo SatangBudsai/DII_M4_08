ce86cdc269cae44e7da979eb5a8d28976c55674b
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-05-18 15:08:10 +0700

    console x and y all

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 var newText = 'new Text';
 document.getElementById('ok').innerHTML = newText;
 document.getElementById('no').innerHTML = '<b><i>Satang</i></b>';
+document.getElementById('ok').addEventListener('click',function(e){
+    // alert('ok is clicked');
+    console.log(e);
+});
+document.getElementById('no').addEventListener('mousemove',function(e){
+    console.log('x : '+e.screenX+' y : '+e.screenY);
+});
+
+// console x and y all
+let elems = document.getElementsByClassName('flex-item');
+console.log(elems);
+    for(elem of elems){
+        elem.addEventListener('mousemove',function(e){
+        console.log('x : '+e.screenX+' y : '+e.screenY);
+    });
+}
+
