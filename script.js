0a3a80b05922080fdd57b35aaa69445a85e75d90
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-05-19 13:15:48 +0700

    event doubleclick 'no' showText

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-var newText = 'new Text';
+var newText = 'OK';
 document.getElementById('ok').innerHTML = newText;
-document.getElementById('no').innerHTML = '<b><i>Satang</i></b>';
+document.getElementById('no').innerHTML = '<b><i>NO</i></b>';
 document.getElementById('ok').addEventListener('click',function(e){
     // alert('ok is clicked');
     console.log(e);
 });
 document.getElementById('no').addEventListener('mousemove',function(e){
-    console.log('x : '+e.screenX+' y : '+e.screenY);
+    // console.log('x : '+e.screenX+' y : '+e.screenY);
 });
 
 // console x and y all
 let elems = document.getElementsByClassName('flex-item');
 console.log(elems);
     for(let elem of elems){
-        elem.addEventListener('mousemove',function(e){
-        console.log('x : '+e.screenX+' y : '+e.screenY);
-        });
+        // elem.addEventListener('mousemove',function(e){
+        // console.log('x : '+e.screenX+' y : '+e.screenY);
+        // });
         // console x and y all
         elem.addEventListener('click',function(e){
-        alert(elem.innerText);
+        // alert(elem.innerText);
 
         });
 }
@@ -33,4 +33,26 @@ document.getElementById('ok').addEventListener('click',function(e){
     }
 })
 
+var borderTick = 10;
+document.getElementById('cancel').style.border = `${borderTick}px solid white` ;
 
+let margin =20;
+document.getElementById('ok').addEventListener('click',function(e){
+    margin += 5;
+    var marginSize =`20px ${margin}px`;
+    document.getElementById('cancel').style.margin =marginSize;
+    document.getElementById('no').style.margin =marginSize;
+});
+
+
+let showText = ['ok','วิชานี้','ง่าย','จริง ๆ นะจ๊ะ'];
+let showIndex = 0;
+document.getElementById('no').addEventListener('dblclick',function(e){
+    showIndex++;
+    if(showIndex >= showText.length){
+        showIndex =0;
+    }
+    console.log(`show index = ${showIndex}`)
+    const okButton = document.getElementById('ok')
+    okButton.innerText = showText[showIndex]
+})
